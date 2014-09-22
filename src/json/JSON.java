1c90bb5ff3f78ac9674e41138656eb14648b63b0
palass <pa.lassonde@gmail.com>
2014-09-22 10:19:32 -0400

    erreur de librairie

--- src/json/JSON.java
+++ src/json/JSON.java
@@ -9,5 +9,14 @@ import net.sf.json.JSONObject;
 public class JSON {
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
+        
+        String json = FileReader.loadFileIntoString("json/employe.json", "UTF-8");
+        JSONArray employes = JSONArray.fromObject(json);
+        
+        int nbEmployes = employes.size();
+        for(int i=0; i<nbEmployes; i++){
+            System.out.println(employes.get(i));
+        }
+        
     }
 }
